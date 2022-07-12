668266d7bd2cc8278ae52168a70bf6ea3cc35b9f
roger.miranda <roger.miranda@students.salle.url.edu>
2022-07-12 17:47:16 +0200

    Feature: load custom blocks onEnable

--- src/com/rogermiranda1000/helper/RogerPlugin.java
+++ src/com/rogermiranda1000/helper/RogerPlugin.java
@@ -10,12 +10,14 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.craftbukkit.libs.jline.internal.Nullable;
 import org.bukkit.entity.Player;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public abstract class RogerPlugin extends JavaPlugin implements CommandExecutor {
     public final String clearPrefix = ChatColor.GOLD.toString() + ChatColor.BOLD + "[" + this.getName() + "] " + ChatColor.GREEN,
@@ -23,23 +25,50 @@ public abstract class RogerPlugin extends JavaPlugin implements CommandExecutor 
 
     private final Listener []listeners;
     private final CustomCommand []commands;
-    private final CustomBlock []customBlocks;
+    private final ArrayList<CustomBlock<?>> customBlocks;
+
+    private boolean isRunning;
 
     /**
      * JavaPlugin with some basic functionalities.
      * /!\\ While overriding onEnable remember to call the super() function /!\\
      * @param commands      All the commands from the plugin. It's important that in the first position you set the 'help' command
-     * @param customBlocks  Plugin's custom blocks
      * @param listeners     All the event listeners from the plugin
      */
-    public RogerPlugin(CustomCommand []commands, CustomBlock []customBlocks, Listener... listeners) {
+    public RogerPlugin(CustomCommand []commands, Listener... listeners) {
+        this.customBlocks = new ArrayList<>();
+        this.isRunning = false;
         this.commands = commands;
-        this.customBlocks = customBlocks;
         this.listeners = listeners; // Listener... is the same than Listener[]
     }
 
-    public RogerPlugin(CustomCommand []commands, Listener... listeners) {
-        this(commands, new CustomBlock[]{}, listeners);
+    /**
+     * Called to add a new custom block
+     * @return Method concatenation
+     */
+    // TODO on stop remove all?
+    public RogerPlugin addCustomBlock(CustomBlock<?> cb) {
+        this.customBlocks.add(cb);
+
+        if (this.isRunning) {
+            cb.load();
+            Bukkit.getPluginManager().registerEvents(cb, this);
+        }
+
+        return this;
+    }
+
+    /**
+     * Called while the server is running if the block must be removed (DON'T call this onStop)
+     * @return Method concatenation
+     */
+    public RogerPlugin removeCustomBlock(CustomBlock<?> cb) {
+        cb.save();
+        HandlerList.unregisterAll(cb);
+
+        this.customBlocks.remove(cb);
+
+        return this;
     }
 
     public void printConsoleErrorMessage(String msg) {
@@ -63,6 +92,8 @@ public abstract class RogerPlugin extends JavaPlugin implements CommandExecutor 
 
     @Override
     public void onEnable() {
+        this.isRunning = true;
+
         // TODO any way to save the instance here?
 
         Bukkit.getScheduler().runTaskAsynchronously(this,()->{
@@ -80,7 +111,7 @@ public abstract class RogerPlugin extends JavaPlugin implements CommandExecutor 
         // register the events
         PluginManager pm = getServer().getPluginManager();
         for (Listener lis : this.listeners) pm.registerEvents(lis, this); // TODO ignore some events depending on the version
-        for (CustomBlock cb : this.customBlocks) pm.registerEvents(cb, this);
+        for (Listener lis : this.customBlocks) pm.registerEvents(lis, this);
 
         if (this.commands.length > 0 && VersionController.version.compareTo(Version.MC_1_10) >= 0) {
             // if MC > 10 we can send hints onTab
@@ -89,13 +120,15 @@ public abstract class RogerPlugin extends JavaPlugin implements CommandExecutor 
         }
 
         // call enable functions
-        for (CustomBlock cb : this.customBlocks) cb.load();
+        for (CustomBlock<?> cb : this.customBlocks) cb.load();
     }
 
     @Override
     public void onDisable() {
+        this.isRunning = false;
+
         // call disable functions
-        for (CustomBlock cb : this.customBlocks) cb.save();
+        for (CustomBlock<?> cb : this.customBlocks) cb.save();
     }
 
     @Override
