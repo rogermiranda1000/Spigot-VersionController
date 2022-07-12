537e3edfd8edb5e299d6fbe1e0de01d217ffb4c5
roger.miranda <roger.miranda@students.salle.url.edu>
2022-07-12 17:24:21 +0200

    Feature: CustomBlocks util functions

--- src/com/rogermiranda1000/helper/CustomBlock.java
+++ src/com/rogermiranda1000/helper/CustomBlock.java
@@ -3,6 +3,7 @@ package com.rogermiranda1000.helper;
 import com.github.davidmoten.rtreemulti.Entry;
 import com.github.davidmoten.rtreemulti.RTree;
 import com.github.davidmoten.rtreemulti.geometry.Point;
+import com.github.davidmoten.rtreemulti.internal.EntryDefault;
 import com.rogermiranda1000.versioncontroller.VersionController;
 import com.rogermiranda1000.versioncontroller.blocks.BlockType;
 import org.bukkit.Location;
@@ -14,6 +15,7 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.function.Consumer;
 import java.util.function.Function;
@@ -57,11 +59,21 @@ public abstract class CustomBlock<T> implements Listener {
     }
 
     @EventHandler(ignoreCancelled = true)
+    public void onBlockPlace(BlockPlaceEvent e) {
+        Block b = e.getBlock();
+        if (!this.isTheSameCustomBlock.apply(b)) return;
+
+        T add = this.onCustomBlockPlace(e, this);
+        if (e.isCancelled() || add == null) return;
+        this.blocks = this.blocks.add(add, CustomBlock.getPoint(b.getLocation()));
+    }
+
+    @EventHandler(ignoreCancelled = true)
     public void onBlockBreak(BlockBreakEvent e) {
         Block b = e.getBlock();
         if (!this.isTheSameCustomBlock.apply(b)) return;
 
-        T rem = this.getCustomBlock(b.getLocation());
+        T rem = this.getBlock(b.getLocation());
         if (rem == null) throw new RuntimeException("Expecting element at position " + b.getLocation().toString() + ", but instead 'null' found");
 
         this.onCustomBlockBreak(e, this, rem);
@@ -69,23 +81,29 @@ public abstract class CustomBlock<T> implements Listener {
         this.blocks = this.blocks.delete(rem, CustomBlock.getPoint(b.getLocation()));
     }
 
-    @EventHandler(ignoreCancelled = true)
-    public void onBlockPlace(BlockPlaceEvent e) {
-        Block b = e.getBlock();
-        if (!this.isTheSameCustomBlock.apply(b)) return;
+    // TODO onUse, onStep
 
-        T rem = this.onCustomBlockPlace(e, this);
-        if (e.isCancelled() || rem == null) return;
-        this.blocks = this.blocks.add(rem, CustomBlock.getPoint(b.getLocation()));
+    public void placeBlockArtificially(T add, Location loc) {
+        this.blocks = this.blocks.add(add, CustomBlock.getPoint(loc));
     }
 
-    // TODO onUse, onStep
+    public void placeBlocksArtificially(T add, Iterable<Location> loc) {
+        loc.forEach(l -> this.placeBlockArtificially(add, l));
+    }
+
+    public void placeBlockArtificially(T add, Block block) {
+        this.placeBlockArtificially(add, block.getLocation());
+    }
+
+    /*public void placeBlocksArtificially(T add, Iterable<Block> blocks) {
+        blocks.forEach(b -> this.placeBlockArtificially(add, b));
+    }*/
 
     /**
      * Get all the placed blocks of this type
      * @param blockConsumer Function to execute for each block
      */
-    public void getAllCustomBlocks(final Consumer<T> blockConsumer) {
+    public void getAllBlocks(final Consumer<T> blockConsumer) {
         this.blocks.entries().forEach(e -> blockConsumer.accept(e.value()));
     }
 
@@ -95,22 +113,43 @@ public abstract class CustomBlock<T> implements Listener {
      * @return      Custom block; null if none
      */
     @Nullable
-    public T getCustomBlock(Location loc) {
+    public T getBlock(Location loc) {
         Iterator<Entry<T, Point>> results = this.blocks.search(CustomBlock.getPoint(loc)).iterator();
 
         if (!results.hasNext()) return null;
         return results.next().value();
     }
 
+    public boolean removeBlockArtificially(Location loc) {
+        T rem = this.getBlock(loc);
+        if (rem == null) return false;
+
+        Point pos = CustomBlock.getPoint(loc);
+        this.blocks.delete(rem, pos);
+        return true;
+    }
+
+    /**
+     * Remove from the list all the occurrences of val
+     * Note: T must have implemented a valid 'equals' function
+     * @param val Object to remove
+     */
+    public void removeBlocksArtificiallyByValue(@NotNull final T val) {
+        ArrayList<Entry<T,Point>> rem = new ArrayList<>();
+        this.blocks.entries().forEach(e -> {
+            if (val.equals(e.value())) rem.add( new EntryDefault<>(e.value(), e.geometry()) );
+        });
+        for (Entry<T,Point> r : rem) this.blocks = this.blocks.delete(r);
+    }
+
     /**
      * /!\\ Called BEFORE the object is added to the list /!\\
-     * @return the object to add
+     * @return the object to add (can't be null)
      */
     abstract public T onCustomBlockPlace(BlockPlaceEvent e, CustomBlock<T> cb);
 
     /**
      * /!\\ Called BEFORE the object is removed from the list /!\\
-     * @return the object to remove
      */
     abstract public void onCustomBlockBreak(BlockBreakEvent e, CustomBlock<T> cb, T element);
 }
