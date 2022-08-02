61c5abca50d1d692da8f872b2990fd8631551f43
rogermiranda1000 <rogermirandaperez@hotmail.com>
2022-08-02 16:18:22 +0200

    Feature: default

--- src/com/rogermiranda1000/versioncontroller/entities/EntitySpigotPost14.java
+++ src/com/rogermiranda1000/versioncontroller/entities/EntitySpigotPost14.java
@@ -1,21 +1,8 @@
 package com.rogermiranda1000.versioncontroller.entities;
 
 import org.bukkit.entity.Entity;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.util.Vector;
-import org.jetbrains.annotations.NotNull;
 
 public class EntitySpigotPost14 implements EntityManager {
-    @Override
-    public @NotNull Vector getVelocity(Entity e) {
-        return e.getVelocity();
-    }
-
-    @Override
-    public @NotNull Vector getVelocity(PlayerMoveEvent e) {
-        return e.getPlayer().getVelocity();
-    }
-
     @Override
     public BoundingBox getBoundingBox(Entity e) {
         org.bukkit.util.BoundingBox boundingBox = e.getBoundingBox();
